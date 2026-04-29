05887c6ea1773a3f2316613baa464b2711833d7f
MarySPerez <ms.perezgomez1@gmail.com>
2026-04-29 00:08:44 -0400

    Cambio en las rutas

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -15,112 +15,32 @@ function AppRoute() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <AuthLayout>
-              <Login />
-            </AuthLayout>
-          }
-        />
+        {/* Rutas Públicas */}
+        <Route path="/" element={<AuthLayout><Login /></AuthLayout>} />
+        <Route path="/login" element={<AuthLayout><Login /></AuthLayout>} />
 
-        <Route
-          path="/login"
-          element={
-            <AuthLayout>
-              <Login />
-            </AuthLayout>
-          }
-        />
+        {/* Rutas Admin */}
+        <Route path="/admin/dashboard" element={<PrivateRoute role="admin"><AdminLayout><Dashboard /></AdminLayout></PrivateRoute>} />
+        <Route path="/admin/exhibition" element={<PrivateRoute role="admin"><AdminLayout><Exhibition /></AdminLayout></PrivateRoute>} />
+        
+        {/* Aquí es donde usas CinemasPage para la ruta de sucursales */}
+        <Route path="/admin/sucursales" element={<PrivateRoute role="admin"><AdminLayout><CinemasPage /></AdminLayout></PrivateRoute>} />
+        
+        <Route path="/admin/users" element={<PrivateRoute role="admin"><AdminLayout><Users /></AdminLayout></PrivateRoute>} />
+        
+        {/* Rutas en desarrollo (Admin) */}
+        <Route path="/admin/transacciones" element={<PrivateRoute role="admin"><AdminLayout><div className="p-4">Próximamente: Transacciones</div></AdminLayout></PrivateRoute>} />
+        <Route path="/admin/inventario" element={<PrivateRoute role="admin"><AdminLayout><div className="p-4">Próximamente: Inventario</div></AdminLayout></PrivateRoute>} />
+        <Route path="/admin/reports" element={<PrivateRoute role="admin"><AdminLayout><div className="p-4">Próximamente: Reportes</div></AdminLayout></PrivateRoute>} />
 
-        <Route
-          path="/admin/dashboard"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout>
-                <Dashboard />
-              </AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/admin/exhibition"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout>
-                <Exhibition />
-              </AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-
-        <Route
-          path="/admin/sucursales"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout>
-                <CinemasPage />
-              </AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-
-        <Route
-          path="/admin/users"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout>
-                <Users />
-              </AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/admin/transacciones"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout></AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/admin/inventario"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout></AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/admin/reports"
-          element={
-            <PrivateRoute role="admin">
-              <AdminLayout></AdminLayout>
-            </PrivateRoute>
-          }
-        />
-
-        {/* Rutas privadas cajero */}
+        {/* Rutas Cajero - CORREGIDAS (Eliminé la duplicada) */}
         <Route
           path="/ticketOffice/dashboard"
           element={
             <PrivateRoute role="cashier">
-              <DashboardCashier />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/ticketOffice/dashboard"
-          element={
-            <PrivateRoute role="cashier">
-              <CashierLayout />
+              <CashierLayout>
+                <DashboardCashier />
+              </CashierLayout>
             </PrivateRoute>
           }
         />
@@ -129,7 +49,9 @@ function AppRoute() {
           path="/ticketOffice/sell"
           element={
             <PrivateRoute role="cashier">
-              <SellTickets />
+              <CashierLayout>
+                <SellTickets />
+              </CashierLayout>
             </PrivateRoute>
           }
         />
