a43edad673e8049ee695eb53fd1411d1990ac118
MarySPerez <ms.perezgomez1@gmail.com>
2026-05-20 15:24:30 -0400

    Prueba para la eliminar sucursal

--- tests/cinemas.spec.js
+++ tests/cinemas.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-//Crear una sucursal con SUPERADMIN
+//LOGIN EXITOSO, AHORA CREAREMOS UNA SUCURSAL------------------------------------
 test('Crear sucursal - SUPERADMIN', async ({ page }) => {
   await page.goto('/login');
 
@@ -14,13 +14,13 @@ test('Crear sucursal - SUPERADMIN', async ({ page }) => {
   await page.getByRole('button', { name: 'Iniciar sesión' }).click();
   await expect(page).toHaveURL('/admin/dashboard');
 
-  //LOGIN EXITOSO, AHORA CREAREMOS UNA SUCURSAL------------------------------------
+//CREACION DE UNA SUCURSAL-----------------------------------------------
 
   await page.getByRole('link', { name: 'Sucursales' }).click();
   await page.getByRole('button', { name: 'Nueva sucursal' }).click();
-  await page.getByPlaceholder('Ej: Cine Plaza').fill('Sucursal Prueba');
+  await page.getByPlaceholder('Ej: Cine Plaza').fill('Sucursal Prueba 3');
   await page.getByPlaceholder('Ej: Av. Principal 123').fill('Calle Falsa 123');
-  await page.getByPlaceholder('Ej: 0251 123 1243').fill('0251 556 2546');
+  await page.getByLabel('Teléfono').fill('0251 556 2546');
 
   const bloqueApertura = page.locator('div.flex-col:has(label:text-is("Apertura"))');
   await bloqueApertura.getByRole('button', { name: 'Hora' }).click(); // Seleccionar Hora
@@ -35,8 +35,19 @@ test('Crear sucursal - SUPERADMIN', async ({ page }) => {
   await page.locator('div:text-is("10")').click();
   await bloqueCierre.getByRole('button', { name: 'Min' }).click(); // Seleccionar Minuto
   await page.locator('div:text-is("45")').click();
-  await bloqueCierre.getByRole('button', { name: 'AM' }).click(); //AM/PM
-  await page.locator('div:text-is("PM")').click();
 
-  await page.getByRole('button', { name: 'Guardar' }).click();
+  await page.getByRole('button', { name: 'Registrar' }).click();
+
+  //ELIMINACION DE UNA SUCURSAL-----------------------------------------------
+/*
+  await page.getByRole('link', { name: 'Sucursales' }).click();
+  const sucursalAEliminar = 'Sucursal Prueba 3';
+  const filaSucursal = page.locator('tr').filter({ hasText: sucursalAEliminar });
+  await expect(filaSucursal).toBeVisible();
+  const botonEliminar = filaSucursal.locator('button.text-red-500');
+  await botonEliminar.click();
+  const botonConfirmarBorrado = page.getByRole('button', { name: 'Confirmar' }); // o 'Sí, eliminar'
+  await botonConfirmarBorrado.click();
+*/
+
 });
