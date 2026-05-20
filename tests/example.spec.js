9d155835771450b92183bf30c4f999121ab44629
MarySPerez <ms.perezgomez1@gmail.com>
2026-05-20 10:00:27 -0400

    Cambio a playwright con js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,15 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://front-backoffice-intranet.onrender.com/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Backoffice/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://front-backoffice-intranet.onrender.com/');
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
