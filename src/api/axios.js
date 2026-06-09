398a30900036c6376344572166cf3e0e180705dc
alirio <pastoralirio6589@gmail.com>
2026-06-09 14:48:10 -0400

    fix: corregido error al intentar cerrar sesión donde no se estaba solicitando el cierre

--- src/api/axios.js
+++ src/api/axios.js
@@ -50,7 +50,7 @@ export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-jog6.onrender.com/api/v1", // Usar ruta relativa para que Vite intercepte con su proxy
+  baseURL: "https://127.0.0.1/api/v1",//"https://backend-jog6.onrender.com/api/v1", // Usar ruta relativa para que Vite intercepte con su proxy
   withCredentials: true,     
   headers: {
     "x-client-channel": "web", 
